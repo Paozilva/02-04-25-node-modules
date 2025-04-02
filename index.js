acfac5fc935853d40b4b79d203c348cefb66f169
Paozilva <asesoriadetareas315@gmail.com>
2025-04-02 17:02:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: 'senac@02',
-    database: 'sakila'
+    database: 'silva'
 }).promise();
 
-const result = await pool.query("select * from `sakila`.`country`;");
+const result = await pool.query("insert into `silva`.`tbl_login` (`nome`, `login`, `senha`) values ('teste', 'teste@teste','mudar123');");
 
 console.log(result);
 
